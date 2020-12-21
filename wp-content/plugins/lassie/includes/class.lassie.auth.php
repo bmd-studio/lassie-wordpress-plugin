a7bbf86974b2c500d051bb1985fb4eacec27ab01
Pepijn Verburg <pepijn@moeilijkedingen.nl>
2020-12-21 10:21:18 +0100

    fix: added try-catch clause to the `lassie_authenticate` Wordpress hook to make sure that any authentication errors will now properly return an empty user, causing Wordpress to properly continue with its internal validation methods.

--- wp-content/plugins/lassie/includes/class.lassie.auth.php
+++ wp-content/plugins/lassie/includes/class.lassie.auth.php
@@ -13,11 +13,18 @@ class Lassie_Auth {
 
   public function lassie_authenticate($user, $username, $password) {
     // Guard: Check if the username or password is empty, or continue.
-    if($username == '' || $password == '') return;
+    if ($username == '' || $password == '') return;
 
     // Check the username and password, and return if there is no result
-    $result = Lassie::getPersonKeys($username, $password);
-    if(isset($result->error) || $result == false) return;
+    $result = FALSE;
+
+    try {
+      $result = Lassie::getPersonKeys($username, $password);
+    } catch (Exception $e) {
+      // empty
+    }
+
+    if (isset($result->error) || $result == FALSE) return;
 
     // Get the WP-user with the retrieved targetID
     $user = new WP_User('', $result->target_id);
